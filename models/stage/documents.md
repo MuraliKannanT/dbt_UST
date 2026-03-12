b9d89a949b61bb14a82a982d8ceb7cc1c7509651
MuraliKannanT <muralikannan25@gmail.com>
2026-03-12 14:57:10 +0530

    Upto Incremental

---
+++ models/stage/documents.md
@@ -0,0 +1,2 @@
+{% docs primary %} This is the primary key for this table {% enddocs %}
+{% docs tests %} This is a another test {% enddocs %}
--- models/stage/stg_nations.sql
+++ models/stage/stg_nations.sql
@@ -2,4 +2,4 @@ select n_nationkey nation_id,
        n_regionkey region_id,
        n_name name,
        n_comment comment
-        from {{ source('src', 'nations') }}
+        from {{ source('src01', 'nations') }}
